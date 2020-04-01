fee93e6c31189ce3a79e2b8beb644ceedec685fb
ishchenkoviacheslav <viacheslav.ishchenko@swanteams.com>
2020-04-01 20:28:35 +0300

    Add project files.

---
+++ GoogleApiTest/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Google.Apis.Auth;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GoogleApiTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task Index(string idtoken)
+        {
+            var validPayload = await GoogleJsonWebSignature.ValidateAsync(idtoken);
+        }
+    }
+}
